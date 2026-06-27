bea5a51db6458dac542dcd1a42e638cb6ce8b181
Md Faizan Ashrafi <mdfaizanashrafi13032001@gmail.com>
2026-06-27 19:04:40 +0530

    pilot

---
+++ src/data/navigation.ts
@@ -0,0 +1,18 @@
+export const navigation = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Philosophy",
+    href: "/philosophy",
+  },
+  {
+    label: "Solutions & Engineering",
+    href: "/solutions_and_engineering",
+  },
+  {
+    label: "Get in Touch",
+    href: "/get_in_touch",
+  },
+];
---
+++ src/data/siteConfig.ts
@@ -0,0 +1,22 @@
+export const siteConfig = {
+  name: "Sovereon",
+  title: "Sovereon",
+  description:
+    "A philosophy-driven software company building thoughtful microsaas and engineering solutions.",
+
+  url: "https://sovereon.online",
+
+  author: "Sovereon",
+
+  socialImage: "/images/og-image.png",
+
+  keywords: [
+    "microsaas",
+    "software engineering",
+    "automation",
+    "consulting",
+    "startup",
+    "technology",
+    "Sovereon"
+  ]
+};
